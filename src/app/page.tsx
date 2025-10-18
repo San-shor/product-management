34c28ddd665bbaebc065625e23fd3e77839af5c2
san-shor <sanjidaewu89@gmail.com>
2025-10-18 19:52:15 +0600

    feat: implement client-side redirection in Home component based on authentication token, routing to products or login page accordingly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAppSelector } from './lib/redux/store';
+
 export default function Home() {
-  return <div>hello</div>;
+  const token = useAppSelector((state) => state.auth.token);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (token) {
+      router.replace('/products');
+    } else {
+      router.replace('/login');
+    }
+  }, [token, router]);
+
+  return null;
 }
